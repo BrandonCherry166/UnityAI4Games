f5f5a9b873435ca9e661bd23d03d7ac543c4345d
Brandon Cherry <brandon.cherry@mymail.champlain.edu>
2025-09-04 12:44:27 -0400

    All forces working. Now need debug menu and follow the mouse for fun

--- Flocking/Flocking Simulation/Assets/Boid.cs
+++ Flocking/Flocking Simulation/Assets/Boid.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
     [SerializeField] List<Sprite> sprites;
+    [SerializeField] FlockingManager fManager;
     public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        if (fManager == null)
+        {
+            fManager = FindObjectOfType<FlockingManager>();
+        }
         //Choose character randomly (Just for fun)
         this.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+        fManager.boids.Add(this);
+        //rb.velocity = Random.insideUnitCircle.normalized * Random.Range(1f, 2f);
     }
 
     // Update is called once per frame
--- Flocking/Flocking Simulation/Assets/FlockingManager.cs
+++ Flocking/Flocking Simulation/Assets/FlockingManager.cs
@@ -4,38 +4,65 @@ using UnityEngine;
 
 public class FlockingManager : MonoBehaviour
 {
-    [SerializeField] double radius;
-    [SerializeField] float k;
+    [SerializeField] float radius;
+
+    [SerializeField] float kCohesion;
+    [SerializeField] float kAlignment;
+    [SerializeField] float kSeparation;
+
     [SerializeField] float maxForce;
-    private GameObject [] boids;
-    List<Boid> currentState, newState;
+    [SerializeField] float maxSpeed;
+    public List <Boid> boids;
     // Start is called before the first frame update
     void Start()
     {
-        boids = GameObject.FindGameObjectsWithTag("Boid");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        for (int i = 0; i < currentState.Count; i++)
+        for (int i = 0; i < boids.Count; i++) 
         {
-            Vector2 cohesionForce = ComputeCohesion(i);
-            Vector2 allignmentForce = ComputeAllignment(i);
-            Vector2 separationForce = ComputeSeparation(i);
+            Rigidbody2D rb = boids[i].GetComponent<Rigidbody2D>();
+            Vector2 cohesion = ComputeCohesion(i);
+            Vector2 alignment = ComputeAlignment(i);
+            Vector2 separation = ComputeSeparation(i);
 
-            Vector2 totalForce = cohesionForce + allignmentForce + separationForce;
-            newState[i].GetComponent<Rigidbody2D>().velocity = currentState[i].GetComponent<Rigidbody2D>().velocity + totalForce * Time.deltaTime;
-            newState[i].GetComponent<Rigidbody2D>().position = currentState[i].GetComponent<Rigidbody2D>().position = newState[i].GetComponent<Rigidbody2D>().velocity * Time.deltaTime;
+            Vector2 totalForce = cohesion + alignment + separation;
+            Debug.Log("Force: " + totalForce + " Vel: " + rb.velocity);
+            Debug.Log("Cohesion: " + cohesion);
+            Debug.Log("Alignment: " + alignment);
+            Debug.Log("Separation: " + separation);
+            rb.velocity += totalForce * Time.fixedDeltaTime;
+
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+            }
+
+            rb.MovePosition(rb.position + rb.velocity * Time.fixedDeltaTime);
+
+
+            //Force to screen space
+            Vector2 screenMin = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+            Vector2 screenMax = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+            float margin = 0.5f; // how close to the edge before steering
+
+            Vector2 steer = Vector2.zero;
+            if (rb.position.x < screenMin.x + margin) steer.x = 1;
+            if (rb.position.x > screenMax.x - margin) steer.x = -1;
+            if (rb.position.y < screenMin.y + margin) steer.y = 1;
+            if (rb.position.y > screenMax.y - margin) steer.y = -1;
+
+            rb.velocity += steer * maxForce;
         }
-        SwitchLists(currentState, newState);
     }
 
     Vector2 ComputeCohesion(int currentBoidIndex)
     {
         List<Vector2> positions = new List<Vector2>();
         Vector2 currentPos = new Vector2(boids[currentBoidIndex].GetComponent<Rigidbody2D>().position.x, boids[currentBoidIndex].GetComponent<Rigidbody2D>().position.y);
-        for (int i = 0; i < boids.Length; i++)
+        for (int i = 0; i < boids.Count; i++)
         {
             if (i == currentBoidIndex)
             { //Skip Comparing with Self
@@ -61,7 +88,7 @@ public class FlockingManager : MonoBehaviour
 
             if (finalForceVector.magnitude > 0)
             {
-                finalForceVector = finalForceVector.normalized * k;
+                finalForceVector = finalForceVector.normalized * kCohesion;
             }
 
             return finalForceVector;
@@ -69,11 +96,11 @@ public class FlockingManager : MonoBehaviour
         return new Vector2(0, 0);
     }
 
-    Vector2 ComputeAllignment(int currentBoidIndex)
+    Vector2 ComputeAlignment(int currentBoidIndex)
     {
         Vector2 totalVelocity = new Vector2(0, 0);
         int numNeighbors = 0;
-        for (int i = 0; i < boids.Length; i++)
+        for (int i = 0; i < boids.Count; i++)
         {
             if ((boids[currentBoidIndex].GetComponent<Rigidbody2D>().position - boids[i].GetComponent<Rigidbody2D>().position).magnitude <= radius)
             {
@@ -83,16 +110,16 @@ public class FlockingManager : MonoBehaviour
         }
         if (numNeighbors == 0)
         {
-            return boids[currentBoidIndex].GetComponent<Rigidbody2D>().velocity;
+            return Vector2.zero;
         }
         Vector2 avgVelocity = totalVelocity / (float)numNeighbors;
-        return avgVelocity * k;
+        return avgVelocity * kAlignment;
     }
 
     Vector2 ComputeSeparation(int currentBoidIndex)
     {
         Vector2 totalForce = new Vector2(0,0);
-        for (int i = 0; i < boids.Length; i++)
+        for (int i = 0; i < boids.Count; i++)
         {
             if (i == currentBoidIndex)
             {
@@ -107,19 +134,12 @@ public class FlockingManager : MonoBehaviour
                 totalForce += direction;
             }
         }
-        totalForce = totalForce * k;
+        totalForce = totalForce * kSeparation;
         if (totalForce.magnitude > maxForce)
         {
             totalForce = totalForce.normalized * maxForce;
         }
         return totalForce;
-    }
-
-    void SwitchLists (List <Boid> list1, List<Boid> list2)
-    {
-        List<Boid> tempList = list1;
-        list1 = list2;
-        list2 = tempList;
     }
 
 }
