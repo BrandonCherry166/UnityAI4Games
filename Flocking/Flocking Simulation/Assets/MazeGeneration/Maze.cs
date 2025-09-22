5e6bcb6caefc762e7f64cb3c797dca33ea1f053b
Brandon Cherry <brandon.cherry@mymail.champlain.edu>
2025-09-22 16:58:16 -0400

    Maze class conversion completed

--- Flocking/Flocking Simulation/Assets/MazeGeneration/Maze.cs
+++ Flocking/Flocking Simulation/Assets/MazeGeneration/Maze.cs
@@ -60,4 +60,85 @@ public class Maze : MonoBehaviour
     {
         return visitedCells[CellIndex(row, col)];
     }
+
+    List<Neighbor> GetVisitableNeighbors(int row, int col)
+    {
+        var neighbors = new List<Neighbor>();
+
+        if (row > 0 && !IsVisited(row - 1, col)) //Up
+        {
+            neighbors.Add(new Neighbor(row - 1, col, 'U'));
+        }
+
+        if (col + 1 < width && !IsVisited(row, col + 1)) //Right
+        {
+            neighbors.Add(new Neighbor(row, col + 1, 'R'));
+        }
+
+        if (row + 1 < height && !IsVisited(row + 1, col)) // Down
+        {
+            neighbors.Add(new Neighbor(row + 1, col, 'D'));
+        }
+
+        if (col > 0 && !IsVisited(row, col - 1)) //Left
+        {
+            neighbors.Add(new Neighbor(row, col - 1, 'L'));
+        }
+
+        return neighbors;
+    }
+
+    void RemoveWall(int row, int col, Neighbor nb)
+    {
+        switch (nb.direction)
+        {
+            case 'U':
+                horizontalWalls[HWallIndex(row - 1, col)] = false;
+                break;
+            case 'R':
+                verticalWalls[VWallIndex(row, col)] = false;
+                break;
+            case 'D':
+                horizontalWalls[HWallIndex(row, col)] = false;
+                break;
+            case 'L':
+                verticalWalls[VWallIndex(row, col - 1)] = false;
+                break;
+        }
+    }
+
+    public void Generate()
+    {
+        var stack = new Stack<(int row, int col)>();
+        int row = 0, col = 0;
+        MarkVisited(row, col);
+        stack.Push((row,col));
+
+        while (stack.Count > 0)
+        {
+            (int curRow, int curCol) = stack.Peek();
+            var neighbors = GetVisitableNeighbors(curRow, curCol);
+
+            if (neighbors.Count == 0)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                Neighbor chosen;
+                if (neighbors.Count == 1)
+                {
+                    chosen = neighbors[0];
+                }
+                else
+                {
+                    chosen = neighbors[rng.Next(neighbors.Count)];
+                }
+
+                RemoveWall(curRow, curCol, chosen);
+                MarkVisited(chosen.row, chosen.col);
+                stack.Push((chosen.row, chosen.col));
+            }
+        }
+    }
 }
