271ffd44fe569f37a3d6f477d2353c4f5b1c4476
Brandon Cherry <brandon.cherry@mymail.champlain.edu>
2025-09-22 17:03:27 -0400

    Maze Generator implemented

--- Flocking/Flocking Simulation/Assets/MazeGeneration/MazeGenerator.cs
+++ Flocking/Flocking Simulation/Assets/MazeGeneration/MazeGenerator.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject wallPrefab;
+    public float cellSize = 1.0f;
+    public int width = 10, height = 10;
+
+    private void Start()
     {
-        
+        Maze maze = new Maze(width, height);
+        maze.Generate();
+        DrawMaze(maze);
     }
 
-    // Update is called once per frame
-    void Update()
+    void DrawMaze(Maze maze)
     {
-        
+        for (int r = 0; r < maze.height; r++)
+        {
+            for (int c = 0; c < maze.width; c++)
+            {
+                if (r < maze.height - 1 && maze.horizontalWalls[r * maze.width + c])
+                {
+                    Instantiate(wallPrefab, new Vector3(c * cellSize, r * cellSize + cellSize / 2, 0), Quaternion.identity);
+                }
+
+                if (c < maze.width - 1 && maze.verticalWalls[r * (maze.width - 1) + c])
+                {
+                    Instantiate(wallPrefab, new Vector3(c * cellSize + cellSize / 2, r * cellSize, 0), Quaternion.Euler(0,0,90));
+                }
+            }
+        }
     }
 }
